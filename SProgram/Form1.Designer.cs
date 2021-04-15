49124aff60d4656432e2e3c0d25a62e8f5d97000
Карина Дроздова <K.Drozd33@yandex.ru>
2021-04-15 19:33:26 +0300

    Initial commit

---
+++ SProgram/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace SProgram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.quantity = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(58, 185);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(195, 37);
+            this.result.TabIndex = 1;
+            this.result.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // quantity
+            // 
+            this.quantity.Location = new System.Drawing.Point(58, 133);
+            this.quantity.Name = "quantity";
+            this.quantity.ReadOnly = true;
+            this.quantity.Size = new System.Drawing.Size(195, 20);
+            this.quantity.TabIndex = 3;
+            this.quantity.Visible = false;
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(58, 241);
+            this.sum.Name = "sum";
+            this.sum.ReadOnly = true;
+            this.sum.Size = new System.Drawing.Size(195, 20);
+            this.sum.TabIndex = 4;
+            this.sum.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Делители числа:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Сумма делителей:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Количество делителей:";
+            this.label4.Visible = false;
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(110, 58);
+            this.number.Mask = "0000000000";
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(89, 20);
+            this.number.TabIndex = 11;
+            this.number.ValidatingType = typeof(int);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 310);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Калькулятор делителей";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox quantity;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox number;
+    }
+}
+
---
+++ SProgram/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SProgram
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Number = Convert.ToInt32(number.Text);
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                result.Visible = true;
+                sum.Visible = true;
+                quantity.Visible = true;
+                if (Number == 1)
+                {
+                    result.Text = "1";
+                    sum.Text = "1";
+                    quantity.Text = "1";
+                }
+                else if (Number == 0)
+                    MessageBox.Show("Делителем 0 является любое число, отличное от него", "Сообщение");
+                else
+                {
+                    int k = 2;
+                    int summa = 1 + Number;
+                    result.Text = "1" + ",";
+                    for (int i = 2; i < Number; i++)
+                    {
+                        if (Number % i == 0)
+                        {
+                            result.Text += " " + i + ",";
+                            k++;
+                            summa += i;
+                        }
+                    }
+                    result.Text += " " + Number;
+                    quantity.Text = Convert.ToString(k);
+                    sum.Text = Convert.ToString(summa);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите число!", "Ошибка");
+            }
+        }
+    }
+}
